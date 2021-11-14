9481a5f8c2d43aeb09904039381acf30684153c4
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2021-11-13 19:02:17 -0800

    Add if else logic to survey question buttons

--- js/scripts.js
+++ js/scripts.js
@@ -4,57 +4,71 @@ $(document).ready(function() {
     $("#header, #question-two, #question-three, #question-four, #question-five-fe, #question-five-be").hide();
   });
   $("button#q1").click(function() {
-    $("#question-two").show();
-    $("#question-one").hide();
+    let questionOneInput = $("button#q1").val();
+      if (questionOneInput === "One") {
+        $("#question-two").show();
+        $("#question-one").hide();
+    }
   });
   $("button#q2").click(function() {
-    $("#question-three").show();
-    $("#question-two").hide();
+    let questionTwoInput = $("button#q2").val();
+      if (questionTwoInput === "Two") {
+        $("#question-three").show();
+        $("#question-two").hide();
+    }
   });
   $("button#q3").click(function() {
-    $("#question-four").show();
-    $("#question-three").hide();
+    let questionThreeInput = $("button#q3").val();
+      if (questionThreeInput === "Three") {
+        $("#question-four").show();
+        $("#question-three").hide();
+    }
   });
-  $("button#q4a").click(function() {
-    $("#question-five-fe").show();
-    $("#question-four").hide();
+  $("button#q4").click(function() {
+    let questionFourInput = $("button#q4").val();
+    if (questionFourInput === "A") {
+      $("#question-five-fe").show();
+      $("#question-four").hide();
+    } else if (questionFourInput === "B") {
+        $("#question-five-be").show();
+        $("#question-four").hide();
+    }
+  });  
+  $("button#q5-fe").click(function() {
+    let questionFeInput = $("button#q5-fe").val();
+    if (questionFeInput === "A") {
+      $("#survey-results").show();
+      $("#front-end-b, #back-end-a, #back-end-b, #question-five-fe, #contact-info").hide();  
+    } else if (questionFeInput === "B") {
+      $("#survey-results").show();
+      $("#front-end-a, #back-end-a, #back-end-b, #question-five-fe, #contact-info").hide();
+    }
   });
-  $("button#q4b").click(function() {
-    $("#question-five-be").show();
-    $("#question-four").hide();
-  });
-  $("button#q5-fe-a").click(function() {
-    $("#survey-results").show();
-    $("#front-end-b, #back-end-a, #back-end-b, #question-five-fe, #contact-info").hide();
-  });
-  $("button#q5-fe-b").click(function() {
-    $("#survey-results").show();
-    $("#front-end-a, #back-end-a, #back-end-b, #question-five-fe, #contact-info").hide();
-  });
-  $("button#q5-be-a").click(function() {
-    $("#survey-results").show();
-    $("#front-end-a, #front-end-b, #back-end-b, #question-five-be, #contact-info").hide();
-  });
-  $("button#q5-be-b").click(function() {
-    $("#survey-results").show();
-    $("#front-end-a, #front-end-b, #back-end-a, #question-five-be, #contact-info").hide();
+  $("button#q5-be").click(function() {
+    let questionBeInput = $("button#q5-be").val();
+    if (questionBeInput === "A") {
+      $("#survey-results").show();
+      $("#front-end-a, #front-end-b, #back-end-b, #question-five-be, #contact-info").hide();
+    } else if (questionBeInput === "B") {
+      $("#survey-results").show();
+      $("#front-end-a, #front-end-b, #back-end-a, #question-five-be, #contact-info").hide();
+    }
   });
   $("button#next").click(function() {
     $("#survey-results").hide();
     $("#contact-info").show();
-  });
+   });
   $("#contact-info").submit(function(event) {
     event.preventDefault();
     const firstName = $("input#first-name").val();
     const lastName = $("input#last-name").val();
     const emailOne = $("input#email").val();
-
+  
     $(".firstname1").text(firstName);
     $(".lastname1").text(lastName);
     $(".email1").text(emailOne);
-
+  
     $("#contact-info").hide();
     $("#follow-up").show();
   });
 });
-
